0086fc6b334934abfe94ad848c4280f040efb224
J0WI <sjw@gmx.ch>
2015-11-18 19:26:51 +0100

    use https

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
+current="$(curl -sSL 'https://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
 
 upstream="$current"
 if [[ "$current" != *.*.* ]]; then
